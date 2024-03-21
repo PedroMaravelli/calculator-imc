f7fe53789a96363f30e37bb58867a4a82e1bdedf
PedroMaravelli <phmaravelli123@gmail.com>
2024-03-21 19:12:02 -0300

    'feature-add-image

--- src/pages/Calculator/index.tsx
+++ src/pages/Calculator/index.tsx
@@ -1,8 +1,16 @@
-import { ContainerCalculator } from "./style";
+import { CardCalculator, ContainerCalculator, ImgPeoples } from "./style";
+import imagePeoples from '../../assets/ilustration.svg'
 
 export function Calculator (){
     return (
         <ContainerCalculator>
+            <ImgPeoples src={imagePeoples} alt="image of peoples pratice sports"  />
+            <CardCalculator>
+                <div>
+                    <strong>Calculadora - IMC</strong>
+
+                </div>
+            </CardCalculator>
 
         </ContainerCalculator>
     )
--- src/pages/Calculator/style.ts
+++ src/pages/Calculator/style.ts
@@ -1,4 +1,20 @@
 import styled from "styled-components";
 
 export const ContainerCalculator = styled.div`
+display: flex;
+height: 100vh;
+justify-content: center;
+align-items: center;
+background-color: #18181b;
+
+`
+export const CardCalculator = styled.div`
+display: flex;
+gap: 1rem;
+border-radius: 8px;
+
+`
+export const ImgPeoples = styled.img`
+width: 35%;
+
 `
