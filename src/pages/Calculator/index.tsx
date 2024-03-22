1428048bc09900cb54701bf467e28dce92b0eb5e
PedroMaravelli <phmaravelli123@gmail.com>
2024-03-22 17:55:42 -0300

    'feature-finally-imc-page'

--- src/pages/Calculator/index.tsx
+++ src/pages/Calculator/index.tsx
@@ -1,15 +1,37 @@
-import { CardCalculator, ContainerCalculator, ImgPeoples } from "./style";
+import { ButtonCalc, CardCalculator, CardWeight, ContainerCalculator, ContentCardCalculator, ImgPeoples, LinkSiteImc } from "./style";
 import imagePeoples from '../../assets/ilustration.svg'
+import { ArrowSquareOut, HandbagSimple, Ruler } from "phosphor-react";
 
 export function Calculator (){
     return (
         <ContainerCalculator>
             <ImgPeoples src={imagePeoples} alt="image of peoples pratice sports"  />
             <CardCalculator>
-                <div>
-                    <strong>Calculadora - IMC</strong>
+                <strong>Calculadora - IMC</strong>
+                <ContentCardCalculator>
+                   
+                    <p>Peso em KG</p>
+                    <CardWeight>
+                        <HandbagSimple size={32} />
+                        <input type="number" name="weight"  />
+                        <p>Kg</p>
 
-                </div>
+                    </CardWeight>
+                        <p>Altura em metros</p>
+                    <CardWeight>
+                        <Ruler size={32} />
+                        <input type="number" name="weight"  />
+                        <p>m</p>
+
+                    </CardWeight>
+
+                    <ButtonCalc>Calcular</ButtonCalc>
+
+                    
+
+                </ContentCardCalculator>
+
+                <LinkSiteImc href='https://mundoeducacao.uol.com.br/saude-bem-estar/imc.htm'>Mais informações sobre o IMC<ArrowSquareOut size={24} /></LinkSiteImc>
             </CardCalculator>
 
         </ContainerCalculator>
--- src/pages/Calculator/style.ts
+++ src/pages/Calculator/style.ts
@@ -10,11 +10,77 @@ background-color: #18181b;
 `
 export const CardCalculator = styled.div`
 display: flex;
+flex-direction: column;
 gap: 1rem;
+justify-content: center;
 border-radius: 8px;
+margin-left: 5rem;
+width: 30%;
+background-color:#262626 ;
+padding: 2rem 3rem;
+gap: 2rem;
 
+strong{
+    color: white;
+    font-size: 1.5rem;
+}
 `
 export const ImgPeoples = styled.img`
 width: 35%;
+`
+
+export const ContentCardCalculator = styled.div`
+display: flex;
+flex-direction: column;
+width: 100%;
+color:#cbd5e1 ;
+gap: 0.5rem;
+font-size: 1.2rem ;
+border-bottom: gray 1px solid;
+padding-bottom: 2rem;
 
 `
+export const CardWeight = styled.div`
+background-color: #3f3f46;
+border-radius: 3px ;
+padding: 0.5rem;
+width: 100%;
+display: flex;
+justify-content: space-between;
+align-items: center;
+margin-bottom: 1rem;
+
+
+input{
+    width: 90%;
+    background-color: transparent;
+    margin-left: 0.9rem;
+    border: none;
+    outline: none;
+    color: white;
+    font-size: 1.2rem;
+
+}
+
+`
+export const ButtonCalc = styled.button`
+width: 100%;
+padding: 0.7rem;
+background-color: #5a189a;
+border: none;
+color: white;
+font-weight: bold;
+font-size: 1.2rem;
+border-radius: 4px;
+`
+
+export const LinkSiteImc = styled.a`
+display: flex;
+align-items: center;
+justify-content: center;
+gap: 0.5rem;
+text-decoration: none;
+font-weight: 600;
+`
+
+
